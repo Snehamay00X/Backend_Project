334787648efacf3ffe45ff227c2b258422801103
Snehamay00X <snehamayhembram007@gmail.com>
2025-12-03 11:51:33 +0530

    Add comments to subscription model for clarity

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,13 +2,13 @@ import mongoose,{Schema} from "mongoose";
 
 const subscriptionSchema = new Schema({
     subscriber:{
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId, //// one who is subscribing
         ref: "User"
     },
     channel:{
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,//// to one who subscriber is subscripting
         ref: "User"
     }
 },{timestamps: true})
 
-const Subscription = mongoose.model("Subscription",subscriptionSchema)
+export const Subscription = mongoose.model("Subscription",subscriptionSchema)
